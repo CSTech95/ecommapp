a124f244de1ac253f8f041c5b2e5dddd63b94363
Aser <adocteur@gmail.com>
2023-06-21 22:09:43 -0400

    Remove Foreign Keys and SQL JOIN, Use rabbitMQ instead

--- order_service/src/models/src/entity/order.ts
+++ order_service/src/models/src/entity/order.ts
@@ -5,18 +5,14 @@ import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Order {
-	@PrimaryGeneratedColumn({ type: "int" })
-	id: number | undefined | string;
+	@PrimaryGeneratedColumn("uuid")
+	id: string;
 
 	@Column({ type: "varchar" })
 	productId: number | undefined | string;
 
 	@Column({ type: "varchar" })
 	userId: number | undefined | string;
-
-	//@OneToOne((type) => User)
-	//@JoinColumn()
-	//userId: User;
 
 	@Column({ type: "varchar" })
 	shoppingCartId: number | undefined | string;
