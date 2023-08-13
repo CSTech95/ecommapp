a062ea6d5b6b39bcab9b1944c23e1df867b56def
Aser <adocteur@gmail.com>
2023-08-12 23:19:27 -0400

    Docker Compose fixes & Tweaks

--- shipping_service/src/routes/getAllShipments.ts
+++ shipping_service/src/routes/getAllShipments.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express";
-import amqp from "amqplib";
+//import amqp from "amqplib";
 //import { Shipping } from "../models/shipping";
 //import { AppDataSource } from "../index";
 
