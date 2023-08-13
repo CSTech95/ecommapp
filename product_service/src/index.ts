ef2b5092311bb94b5233e82fb783938e02394cca
Aser <adocteur@gmail.com>
2023-08-12 22:03:31 -0400

    Added product_service to docker compose yml file

--- product_service/src/index.ts
+++ product_service/src/index.ts
@@ -7,7 +7,7 @@ import { Product } from "./models/product";
 //TODO Connect to PostgreSQL DB with TypeORM
 export const AppDataSource = new DataSource({
 	type: "postgres",
-	host: process.env.DB_HOST,
+	host: process.env.PGHOST,
 	port: 5432,
 	username: process.env.DB_USERNAME,
 	password: process.env.DB_PASSWORD,
