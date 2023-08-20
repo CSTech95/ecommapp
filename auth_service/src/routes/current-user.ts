0ef8ec6ee88316d82a29d74ae15a12e665d01237
Aser <adocteur@gmail.com>
2023-08-19 22:54:20 -0400

    attached current-user middleware from custom npm package

--- auth_service/src/routes/current-user.ts
+++ auth_service/src/routes/current-user.ts
@@ -4,7 +4,7 @@ import { currentUser, requireAuth } from "@adecomm/common";
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", (req: Request, res: Response) => {
+router.get("/api/users/currentuser", currentUser, (req: Request, res: Response) => {
 	res.send({ currentUser: req.currentUser || null });
 });
 export { router as currentUserRouter };
