ee7246ed14967b7a901e258c55eb003334339ae3
Aser <adocteur@gmail.com>
2023-08-25 23:04:17 -0400

    Comment Removal for test

--- auth_service/src/routes/get-users.ts
+++ auth_service/src/routes/get-users.ts
@@ -8,10 +8,7 @@ const user = new User();
 router.get("/api/users", async (req: Request, res: Response) => {
 	const userRepositoy = AppDataSource.getRepository(User);
 	const users = await userRepositoy.find();
-	//if (!products) {
-	//	throw new Error("product not found");
-	//}
-	//console.log("All user ", users);
+
 	console.log("Sent all users");
 	res.send(users);
 });
