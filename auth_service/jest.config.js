220c57094ba20daa88a5f37e4cac999dbdfe3508
Aser <adocteur@gmail.com>
2023-08-26 00:21:05 -0400

    Edit: Workflow for Auth Service

--- auth_service/jest.config.js
+++ auth_service/jest.config.js
@@ -1,6 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-	preset: "ts-jest",
+	preset: "../node_modules/ts-jest/jest-preset.js",
 	testEnvironment: "node",
 	setupFilesAfterEnv: ["./src/test/setup.ts"],
 };
